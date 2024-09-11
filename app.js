7338f11e9809569ab2ba2d3400e283907bd7a357
PEDRO SEGANTINI SILVA <24.00798-6@maua.br>
2024-09-11 09:44:03 -0300

    Primeiro Commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const menu = document.querySelector("#menu-mobile");
+const menuLinks = document.querySelector(".nav_menu");
+
+const mobileMenu = () => {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
+};
+
+menu.addEventListener("click", mobileMenu);
+console.log(menu);
+console.log(menuLinks);
